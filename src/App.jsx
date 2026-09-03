be9127b6194ab8b56908800d286c1fb7e3c7fbd2
Pruthvi <prithvirajguttedar@gmail.com>
2026-09-03 17:00:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './components/NavBar.jsx'
+import NavBar from './components/NavBar.jsx'
+function App() {
+
+  return (
+    <main>
+      <NavBar></NavBar>
+    </main>
+  )
+}
+
+export default App
